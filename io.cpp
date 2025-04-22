ae5cf4138ddfd173ff10d867694b5f39eef8ac87
finneng01 <finn.engels01@gmail.com>
2025-04-22 14:37:56 +0200

    Aufgabe 9_3 fertig

--- io.cpp
+++ io.cpp
@@ -1,8 +1,18 @@
 #include "io.h"
 
 int n_einlesen(void) {
-    return (1);
+    int n;
+    do {
+        printf("n eingeben");
+        scanf("%d", &n);
+    } while (n < 0);
+    return (n);
 }
 int x_einlesen(void) {
-    return (2);
+    int x;
+    do {
+        printf("x eingeben");
+        scanf("%d", &x);
+    } while (x < 0);
+    return (x);
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main() {
     int x,y,n;
 
-    n = int(n_einlesen);
-    x = int(x_einlesen);
+    n = n_einlesen();
+    x = x_einlesen();
     y = x_hoch_n (x,n);
 
     printf("%d hoch %d = %d\n", x, n, y);
